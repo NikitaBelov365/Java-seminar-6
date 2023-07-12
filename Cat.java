299c8e6ce4b3981db209cccedbdcabeacdf3c1d7
Nikitabelov365 <nikitabelov365@gmail.com>
2023-07-12 19:50:49 +0300

    class Cat described

---
+++ Cat.java
@@ -0,0 +1,36 @@
+import java.sql.Date;
+
+public class Cat {
+    // Vet Clinic
+    String name;
+    String owner_name;
+    Date birth_year;
+    float height;
+    float weight;
+    String diagnosis;
+    int age;
+
+    // Vistavka
+    //String name;
+    //String owner_name;
+    //Date birth_year;
+    String breed;
+    //String mother;
+   // String father;
+    float rating;
+
+    //Cucklochev
+    //String name;
+    //Date birth_year;
+    //String breed;
+    String actor_umplua;
+    String character;
+    String mother;
+    String father;
+
+    public Cat (String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+}
