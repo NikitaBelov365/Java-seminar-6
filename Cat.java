9540cecb89d02425752edf65c939f8955b444542
Nikitabelov365 <nikitabelov365@gmail.com>
2023-07-12 20:42:12 +0300

    tasks solveds

--- Cat.java
+++ Cat.java
@@ -2,7 +2,7 @@ import java.sql.Date;
 
 public class Cat {
     // Vet Clinic
-    String name;
+    String nameCat;
     String owner_name;
     Date birth_year;
     float height;
@@ -28,9 +28,13 @@ public class Cat {
     String mother;
     String father;
 
-    public Cat (String name, int age, String breed) {
-        this.name = name;
+    public  Cat (String nameCat, int age, String breed) {
+        this.nameCat = nameCat;
         this.age = age;
         this.breed = breed;
+    }
+
+    public String toString() {
+        return nameCat + age + breed;
     }
 }
---
+++ Second.java
@@ -0,0 +1,11 @@
+public class Second {
+    public static void main(String[] args) {
+
+        Cat catsOne = new Cat("null", 2, "null");
+
+        // Cat cat2 = new Cat("Gogo", 3, "Meat");
+        // Cat cat3 = new Cat("Arseniy", 29, "Best");
+        System.out.println(catsOne);
+
+    }
+}
